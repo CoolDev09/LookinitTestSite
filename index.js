0b2621690e08e92e899eb8e1978f9dce3883368b
CoolDev09 <164423241+CoolDev09@users.noreply.github.com>
2024-04-12 13:04:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Function to handle search
 function handleSearch() {
-    const query = document.getElementById('searchInput').value.trim();
+    const query = document.getElementById('searchInput2').value.trim();
     if (query) {
         window.location.href = 'results.html?query=' + encodeURIComponent(query);
     }
 }
 
 // Add event listener to search button
-document.getElementById('searchButton').addEventListener('click', handleSearch);
+document.getElementById('searchButton2').addEventListener('click', handleSearch);
